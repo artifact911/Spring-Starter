c1a300108b796566bd78da00fbe4ca14bcb9175d
artifact911 <artifact911@gmail.com>
2022-12-11 23:49:27 +0300

    xml-based config done

--- src/main/java/com/art/spring/ApplicationRunner.java
+++ src/main/java/com/art/spring/ApplicationRunner.java
@@ -1,25 +1,16 @@
 package com.art.spring;
 
-import com.art.spring.ioc.Container;
-import com.art.spring.service.UserService;
+import com.art.spring.database.pool.ConnectionPool;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var container = new Container();
+        var context = new ClassPathXmlApplicationContext("application.xml");
 
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-        // таким образом мы не управляем созданием объектов
-        // более того, нам их создавать не нужно
-        // нам нужен только UserService.class
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
-
-        var userService = container.get(UserService.class);
+        // clazz -> String -> Map<String, Object>
+        var connectionPool = context.getBean("pool2", ConnectionPool.class);
+        var connectionPoolByName = context.getBean("p4", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }
