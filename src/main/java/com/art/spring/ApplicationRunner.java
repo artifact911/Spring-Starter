ddcf3e47346ec1b959c838893044c5d6274af8fd
artifact911 <artifact911@gmail.com>
2022-12-17 16:14:12 +0300

    lifecycleCallbacks done

--- src/main/java/com/art/spring/ApplicationRunner.java
+++ src/main/java/com/art/spring/ApplicationRunner.java
@@ -7,13 +7,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("application.xml");
+        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
 
-        // clazz -> String -> Map<String, Object>
-        var connectionPoolByName = context.getBean("p3", ConnectionPool.class);
-        System.out.println(connectionPoolByName);
+            // clazz -> String -> Map<String, Object>
+            var connectionPoolByName = context.getBean("p3", ConnectionPool.class);
+            System.out.println(connectionPoolByName);
 
-        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-        System.out.println();
+            var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+            System.out.println();
+        }
     }
 }
