26b199b5c5557f0da28d496c8edb10db711056a9
artifact911 <artifact911@gmail.com>
2022-12-17 15:22:09 +0300

    propertyInjection done

--- src/main/java/com/art/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/art/spring/database/pool/ConnectionPool.java
@@ -8,12 +8,16 @@ public class ConnectionPool {
     private final String userName;
     private final Integer poolSize;
     private final List<Object> args;
-    private final Map<String, Object> properties;
+    private Map<String, Object> properties;
 
     public ConnectionPool(String userName, Integer poolSize, List<Object> args, Map<String, Object> properties) {
         this.userName = userName;
         this.poolSize = poolSize;
         this.args = args;
+        this.properties = properties;
+    }
+
+    public void setProperties(Map<String, Object> properties) {
         this.properties = properties;
     }
 }
---
+++ src/main/java/com/art/spring/service/CompanyService.java
@@ -0,0 +1,10 @@
+package com.art.spring.service;
+
+public class CompanyService {
+
+    private UserService userService;
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+}
