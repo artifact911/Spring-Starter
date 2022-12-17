98ea171e7fb6c57c0e3fb41052057ba8f6f76a1c
artifact911 <artifact911@gmail.com>
2022-12-17 14:53:05 +0300

    factoryMethodInjection done

--- src/main/java/com/art/spring/ApplicationRunner.java
+++ src/main/java/com/art/spring/ApplicationRunner.java
@@ -1,6 +1,7 @@
 package com.art.spring;
 
 import com.art.spring.database.pool.ConnectionPool;
+import com.art.spring.database.repository.CompanyRepository;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
@@ -11,5 +12,8 @@ public class ApplicationRunner {
         // clazz -> String -> Map<String, Object>
         var connectionPoolByName = context.getBean("p3", ConnectionPool.class);
         System.out.println(connectionPoolByName);
+
+        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println();
     }
 }
