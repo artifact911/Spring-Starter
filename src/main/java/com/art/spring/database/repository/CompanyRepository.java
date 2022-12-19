4de3ced1416222dfdac9f55fc6e7572fcf44ac19
artifact911 <artifact911@gmail.com>
2022-12-19 22:42:54 +0300

    BeanPostProcessor p1-BeforeInitialization done

--- src/main/java/com/art/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/art/spring/database/repository/CompanyRepository.java
@@ -1,16 +1,10 @@
 package com.art.spring.database.repository;
 
+import com.art.spring.bpp.InjectBean;
 import com.art.spring.database.pool.ConnectionPool;
 
 public class CompanyRepository {
 
-    private final ConnectionPool connectionPool;
-
-    public CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 }
