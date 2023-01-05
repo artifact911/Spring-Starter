b4320ee1a14ab3fbafb7921a4c7d19492f4758b8
artifact911 <artifact911@gmail.com>
2023-01-05 16:43:04 +0300

    BeanAnnotation done

--- src/main/java/com/art/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/art/spring/config/ApplicationConfiguration.java
@@ -1,7 +1,11 @@
 package com.art.spring.config;
 
+import com.art.spring.database.pool.ConnectionPool;
 import com.art.spring.database.repository.CrudRepository;
+import com.art.spring.database.repository.UserRepository;
 import com.art.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -28,4 +32,26 @@ includeFilters = {
         @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository")
 })
 public class ApplicationConfiguration {
+
+    @Bean("pool2")
+    // Singleton по-умолчанию, но можем менять
+//    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool(username, 20);
+    }
+
+    @Bean
+    public ConnectionPool pool3() {
+        return new ConnectionPool("test-pool", 25);
+    }
+
+    @Bean
+    public UserRepository userRepository2(ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
+
+    @Bean
+    public UserRepository userRepository3() {
+        return new UserRepository(pool3());
+    }
 }
