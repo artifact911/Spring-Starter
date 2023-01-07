3cc6f0fa2eece76bf737a803155d782176fca0d9
artifact911 <artifact911@gmail.com>
2023-01-07 18:01:26 +0300

    move to boot done

--- src/main/java/com/art/spring/ApplicationRunner.java
+++ src/main/java/com/art/spring/ApplicationRunner.java
@@ -1,26 +1,12 @@
 package com.art.spring;
 
-import com.art.spring.config.ApplicationConfiguration;
-import com.art.spring.database.pool.ConnectionPool;
-import com.art.spring.database.repository.CrudRepository;
-import com.art.spring.service.CompanyService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        // переход к классу конфигурации
-//        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
-        try (var context = new AnnotationConfigApplicationContext()) {
-            context.register(ApplicationConfiguration.class);
-            context.getEnvironment().setActiveProfiles("web", "prod");
-            context.refresh();
-            // clazz -> String -> Map<String, Object>
-            var connectionPool = context.getBean("pool1", ConnectionPool.class);
-            System.out.println(connectionPool);
-
-            var companyService = context.getBean(CompanyService.class);
-            System.out.println(companyService.findById(1));
-        }
+        SpringApplication.run(ApplicationRunner.class, args);
     }
 }
