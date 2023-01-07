8d9c41769cefda066f38871947ea88c2c1c78a00
artifact911 <artifact911@gmail.com>
2023-01-07 20:21:24 +0300

    Lombok done

--- src/main/java/com/art/spring/service/CompanyService.java
+++ src/main/java/com/art/spring/service/CompanyService.java
@@ -5,25 +5,19 @@ import com.art.spring.dto.CompanyReadDto;
 import com.art.spring.entity.Company;
 import com.art.spring.listener.entity.AccessType;
 import com.art.spring.listener.entity.EntityEvent;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class CompanyService {
 
     private final CrudRepository<Integer, Company> companyCrudRepository;
     private final UserService userService;
     private final ApplicationEventPublisher eventPublisher;
-
-    public CompanyService(CrudRepository<Integer, Company> companyCrudRepository,
-                          UserService userService,
-                          ApplicationEventPublisher eventPublisher) {
-        this.companyCrudRepository = companyCrudRepository;
-        this.userService = userService;
-        this.eventPublisher = eventPublisher;
-    }
 
     public Optional<CompanyReadDto> findById(Integer id) {
         return companyCrudRepository.findById(id)
--- src/main/java/com/art/spring/service/UserService.java
+++ src/main/java/com/art/spring/service/UserService.java
@@ -3,9 +3,11 @@ package com.art.spring.service;
 import com.art.spring.database.repository.CrudRepository;
 import com.art.spring.database.repository.UserRepository;
 import com.art.spring.entity.Company;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
@@ -14,10 +16,4 @@ public class UserService {
     // т.к. у нас будет несколько реализаций CrudRepository, то при инициализации в данном случае, мы будем искать бин
     // на основании имени параметра нашего конструктора
     private final CrudRepository<Integer, Company> companyRepository;
-
-    public UserService(UserRepository userRepository,
-                       CrudRepository<Integer, Company> companyRepository) {
-        this.userRepository = userRepository;
-        this.companyRepository = companyRepository;
-    }
 }
