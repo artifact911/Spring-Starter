255628383846f251b3e2016ff9bad7a241a4445b
artifact911 <artifact911@gmail.com>
2023-01-12 22:42:40 +0300

    integration test p3 done

--- src/test/java/com/art/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/com/art/spring/integration/service/CompanyServiceIT.java
@@ -1,35 +1,23 @@
 package com.art.spring.integration.service;
 
-import com.art.spring.ApplicationRunner;
+import com.art.spring.config.DatabaseProperties;
 import com.art.spring.dto.CompanyReadDto;
-import com.art.spring.entity.Company;
-import com.art.spring.listener.entity.EntityEvent;
+import com.art.spring.integration.annotation.IT;
 import com.art.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
-//@ExtendWith(SpringExtension.class)
-//@ContextConfiguration(classes = ApplicationRunner.class,
-//initializers = ConfigDataApplicationContextInitializer.class)
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
 public class CompanyServiceIT {
 
     private static final Integer COMPANY_ID = 1;
 
-    @Autowired
-    private CompanyService companyService;
+    private final CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
 
     @Test
     void findById() {
