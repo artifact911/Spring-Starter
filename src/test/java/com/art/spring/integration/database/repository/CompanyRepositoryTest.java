efae198abe429a5f7a7aca212af7f80ac59ab688
artifact911 <artifact911@gmail.com>
2023-01-15 23:55:10 +0300

    Transactional. TestContext done

---
+++ src/test/java/com/art/spring/integration/database/repository/CompanyRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.art.spring.integration.database.repository;
+
+import com.art.spring.database.entity.Company;
+import com.art.spring.integration.annotation.IT;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@IT
+@RequiredArgsConstructor
+@Transactional
+// по умолчанию
+//@Rollback
+@Commit
+class CompanyRepositoryTest {
+
+    private final EntityManager entityManager;
+
+    @Test
+    void findById() {
+        var company = entityManager.find(Company.class, 1);
+        assertNotNull(company);
+
+        assertThat(company.getLocales()).hasSize(2);
+    }
+
+    @Test
+    void save() {
+        var company = Company.builder()
+                .name("Apple")
+                .locales(Map.of(
+                        "ru", "Apple описание",
+                        "en", "Apple description"
+                )).build();
+
+        entityManager.persist(company);
+        assertNotNull(company.getId());
+    }
+}
