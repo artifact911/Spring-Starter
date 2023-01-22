01c7eff12147aa825424eacec0b15c04a075ff4c
artifact911 <artifact911@gmail.com>
2023-01-22 18:51:47 +0300

    Repositories. NamedQuery done

--- src/main/java/com/art/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/art/spring/database/repository/CompanyRepository.java
@@ -2,6 +2,7 @@ package com.art.spring.database.repository;
 
 import com.art.spring.database.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,7 +10,7 @@ import java.util.Optional;
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
     // Заюзать можно: Optional, Entity, Future
-    Optional<Company> findByName(String name);
+    Optional<Company> findByName(@Param("name2") String name);
 
     // Containing - аналог LIKE но с %слеваИсправа
     // IgnoreCase - без учета регистра
