7a4dc3ef2427c16b1263fa58bde17cb39fa924a4
artifact911 <artifact911@gmail.com>
2023-01-22 23:35:14 +0300

    Repositories. special parameters (pageable, sort) done

--- src/main/java/com/art/spring/database/repository/UserRepository.java
+++ src/main/java/com/art/spring/database/repository/UserRepository.java
@@ -2,12 +2,16 @@ package com.art.spring.database.repository;
 
 import com.art.spring.database.entity.Role;
 import com.art.spring.database.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -29,4 +33,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
             "set u.role = :role " +
             "where u.id in (:ids)")
     int updateRole(Role role, Long... ids);
+
+    Optional<User> findTopByOrderByIdDesc();
+
+    List<User> findTop3ByBirthDateBeforeOrderByBirthDateDesc(LocalDate birthDate);
+
+    List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);
+
+    List<User> findAllBy(Pageable pageable);
 }
