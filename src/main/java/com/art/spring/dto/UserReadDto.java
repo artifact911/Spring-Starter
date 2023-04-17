5ed4450cabfae137a4e887c5598f196371c9687b
Korben_Dallas <artifact911@gmail.com>
2023-04-17 17:19:42 +0300

    Model done

---
+++ src/main/java/com/art/spring/dto/UserReadDto.java
@@ -0,0 +1,10 @@
+package com.art.spring.dto;
+
+import lombok.Value;
+
+@Value
+public class UserReadDto {
+
+    Long ig;
+    String username;
+}
--- src/main/java/com/art/spring/http/controller/GreetingController.java
+++ src/main/java/com/art/spring/http/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package com.art.spring.http.controller;
 
 import com.art.spring.database.repository.CompanyRepository;
+import com.art.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
@@ -15,15 +18,35 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1")
+@SessionAttributes({"user"})
 public class GreetingController {
 
+    @GetMapping("/hello/")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
+
+        modelAndView.setViewName("greeting/hello");
+        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+
+        return modelAndView;
+    }
+
+    @GetMapping("/bye")
+    public ModelAndView bye(@SessionAttribute("user") UserReadDto user) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("greeting/bye");
+
+        return modelAndView;
+    }
+
+
+    // перенесли пока, пока развлекаемся с новым /hello
     @GetMapping("/hello/{id}")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
-                              // value в анноташке можно опустить, если нейминг переменной совпадает
-                              @RequestParam Integer age,
-                              @RequestHeader String accept,
-                              @CookieValue("JSESSIONID") String jsessionId,
-                              @PathVariable("id") Integer id) {
+    public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
+                               // value в анноташке можно опустить, если нейминг переменной совпадает
+                               @RequestParam Integer age,
+                               @RequestHeader String accept,
+                               @CookieValue("JSESSIONID") String jsessionId,
+                               @PathVariable("id") Integer id) {
 
         // аналог аннотаций:
         String ageParamValue = request.getParameter("age");
@@ -31,14 +54,6 @@ public class GreetingController {
         Cookie[] cookies = request.getCookies();
 
         modelAndView.setViewName("greeting/hello");
-
-        return modelAndView;
-    }
-
-    @GetMapping("/bye")
-    public ModelAndView bye(ModelAndView modelAndView) {
-
-        modelAndView.setViewName("greeting/bye");
 
         return modelAndView;
     }
