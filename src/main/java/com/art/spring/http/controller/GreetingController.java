453e7994e77d673642017223872040278a5be733
Korben_Dallas <artifact911@gmail.com>
2023-04-17 16:21:05 +0300

    Param, Headers, Cookies done

--- src/main/java/com/art/spring/http/controller/GreetingController.java
+++ src/main/java/com/art/spring/http/controller/GreetingController.java
@@ -2,18 +2,33 @@ package com.art.spring.http.controller;
 
 import com.art.spring.database.repository.CompanyRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1")
 public class GreetingController {
 
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request, CompanyRepository companyRepository) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+                              // value в анноташке можно опустить, если нейминг переменной совпадает
+                              @RequestParam Integer age,
+                              @RequestHeader String accept,
+                              @CookieValue("JSESSIONID") String jsessionId,
+                              @PathVariable("id") Integer id) {
+
+        // аналог аннотаций:
+        String ageParamValue = request.getParameter("age");
+        String acceptHeader = request.getHeader("accept");
+        Cookie[] cookies = request.getCookies();
 
         modelAndView.setViewName("greeting/hello");
 
