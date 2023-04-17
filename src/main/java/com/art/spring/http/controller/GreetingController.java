aefb97701caea7675006f3b3b38645cd09b9f824
Korben_Dallas <artifact911@gmail.com>
2023-04-17 15:51:06 +0300

    ReqMapping done

--- src/main/java/com/art/spring/http/controller/GreetingController.java
+++ src/main/java/com/art/spring/http/controller/GreetingController.java
@@ -2,13 +2,17 @@ package com.art.spring.http.controller;
 
 import com.art.spring.database.repository.CompanyRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/api/v1")
 public class GreetingController {
 
+    @GetMapping("/hello")
     public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request, CompanyRepository companyRepository) {
 
         modelAndView.setViewName("greeting/hello");
@@ -16,6 +20,7 @@ public class GreetingController {
         return modelAndView;
     }
 
+    @GetMapping("/bye")
     public ModelAndView bye(ModelAndView modelAndView) {
 
         modelAndView.setViewName("greeting/bye");
